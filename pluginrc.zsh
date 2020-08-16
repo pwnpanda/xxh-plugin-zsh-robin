fdad9feed25db7ab9d091bb493e99c2402aeb01f
Robin Lunde <robin@wub.no>
2020-08-16 19:46:48 +0900

    Add space for readability

--- pluginrc.zsh
+++ pluginrc.zsh
@@ -1,6 +1,9 @@
 # This script will be executed ON THE HOST when you connect to the host.
 # Put here your functions, environment variables, aliases and whatever you need.
 
+build_dir=$HOME/.xxh/plugins/xxh-plugin-zsh-robin/build
+
 CURR_DIR="$(cd "$(dirname "$0")" && pwd)"
 source $CURR_DIR/.zshrc
+
 sudo run-parts /etc/update-motd.d
