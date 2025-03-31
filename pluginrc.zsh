daa1e88a662ce27188badc6f8eb7df5e1cb2e9df
Robin Lunde <robin.lunde@semaphore.no>
2025-03-31 14:05:45 +0200

    LLM fix

--- pluginrc.zsh
+++ pluginrc.zsh
@@ -6,4 +6,13 @@ build_dir=$HOME/.xxh/plugins/xxh-plugin-zsh-robin/build
 CURR_DIR="$(cd "$(dirname "$0")" && pwd)"
 source $CURR_DIR/.zshrc
 
+# Get the actual home directory (one level up from .xxh)
+ACTUAL_HOME=$(cd $HOME/.. && pwd)
+
+# Change to the actual home directory
+cd $ACTUAL_HOME
+
+# Set HOME environment variable to the actual home (if needed)
+export REAL_HOME=$ACTUAL_HOME
+
 #sudo run-parts /etc/update-motd.d
